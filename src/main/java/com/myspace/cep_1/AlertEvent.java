e23caa1a16681927099bc3ced5ba96d087a19340
admin <>
2020-06-09 09:51:11 +0000

    {/src/main/java/com/myspace/cep_1/AlertEvent.java}

--- src/main/java/com/myspace/cep_1/AlertEvent.java
+++ src/main/java/com/myspace/cep_1/AlertEvent.java
@@ -8,6 +8,7 @@ package com.myspace.cep_1;
 @org.kie.api.definition.type.Role(org.kie.api.definition.type.Role.Type.EVENT)
 @org.kie.api.definition.type.Timestamp("time")
 @org.kie.api.definition.type.Duration("duration")
+@org.kie.api.definition.type.Expires("2d")
 public class AlertEvent implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
